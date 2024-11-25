9db371bfc0b6169578b386300885978b32f1e3e5
Nicholas Martino <nicholas.martino@hotmail.com>
2024-11-25 03:58:49 -0800

    Handle null density

--- src/utils/SpatialStats.ts
+++ src/utils/SpatialStats.ts
@@ -2,14 +2,16 @@ import * as turf from '@turf/turf';
 import { Node } from '../types/Node';
 
 // Weighted mean center
-
 const weightedMeanCenter = (nodes: Node[]) => {
-	const totalDensity = nodes.reduce((sum, node) => sum + node.density, 0);
+	const totalDensity = nodes.reduce(
+		(sum, node) => sum + (node.density ?? 1),
+		0
+	);
 	const weightedCoords = nodes.reduce(
 		(acc, node) => {
 			const weightedPoint = [
-				node.longitude * node.density,
-				node.latitude * node.density,
+				node.longitude * (node.density ?? 1),
+				node.latitude * (node.density ?? 1),
 			];
 			return [acc[0] + weightedPoint[0], acc[1] + weightedPoint[1]];
 		},
@@ -26,14 +28,17 @@ const weightedMeanCenter = (nodes: Node[]) => {
 export const spatialDispersionIndex = (nodes: Node[]): number => {
 	if (nodes.length === 0) return 0;
 
-	const totalDensity = nodes.reduce((sum, node) => sum + node.density, 0);
+	const totalDensity = nodes.reduce(
+		(sum, node) => sum + (node.density ?? 1),
+		0
+	);
 	const center = weightedMeanCenter(nodes);
 
 	const variance = nodes.reduce((sum, node) => {
 		const from = turf.point([node.longitude, node.latitude]);
 		const to = turf.point([center.x, center.y]);
 		const distance = turf.distance(from, to, { units: 'kilometers' });
-		return sum + node.density * distance ** 2;
+		return sum + (node.density ?? 1) * distance ** 2;
 	}, 0);
 
 	return Math.sqrt(variance / totalDensity);
